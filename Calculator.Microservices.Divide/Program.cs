c3bea27f5abcc824ec7edd11173b9f5d327f3746
BattleWarriorXXL <battle.warrior.xxl@gmail.com>
2022-02-02 12:18:01 +0300

    Added Divide microservice

--- Calculator.Microservices.Divide/Program.cs
+++ Calculator.Microservices.Divide/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+using var messageBus = new MessageBus(Environment.GetEnvironmentVariable("BOOTSTRAP_SERVERS") ?? "localhost:9092");
+Task.Run(() => messageBus.SubscribeOnTopic<string>(Topics.ACTION_TOPIC, action =>
+{
+    var expressions = Commands.ParseCommand(action);
+    if (expressions == null)
+    {
+        return;
+    }
 
+    var command = expressions.Value.Item1;
+    if (command != Commands.DIVIDE_COMMAND)
+    {
+        return;
+    }
+
+    var a = expressions.Value.Item2;
+    var b = expressions.Value.Item3;
+
+    messageBus.SendMessage(Topics.RESULT_TOPIC, (a / b).ToString());
+}, CancellationToken.None));
 
 app.Run();
