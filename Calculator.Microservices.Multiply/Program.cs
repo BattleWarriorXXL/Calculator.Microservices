d7ddb4d095996dfb1903de04e9ac24f6be694a2b
BattleWarriorXXL <battle.warrior.xxl@gmail.com>
2022-02-02 12:13:20 +0300

    Added Multiply microservice

--- Calculator.Microservices.Multiply/Program.cs
+++ Calculator.Microservices.Multiply/Program.cs
@@ -1,32 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-var summaries = new[]
+using var messageBus = new MessageBus(Environment.GetEnvironmentVariable("BOOTSTRAP_SERVERS") ?? "localhost:9092");
+Task.Run(() => messageBus.SubscribeOnTopic<string>(Topics.ACTION_TOPIC, action =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var expressions = Commands.ParseCommand(action);
+    if (expressions == null)
+    {
+        return;
+    }
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-});
+    var command = expressions.Value.Item1;
+    if (command != Commands.MULTIPLY_COMMAND)
+    {
+        return;
+    }
+
+    var a = expressions.Value.Item2;
+    var b = expressions.Value.Item3;
+
+    messageBus.SendMessage(Topics.RESULT_TOPIC, (a * b).ToString());
+}, CancellationToken.None));
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
